32830f233b58c86edbb07198f7cc84abff259161
Ashes110 <atsu110121@gmail.com>
2023-01-31 18:10:23 +0900

    タブ等の細かい修正と42Headerの削除

--- src/executer.c
+++ src/executer.c
@@ -1,42 +1,29 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   executer.c                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: takumasaokamoto <takumasaokamoto@studen    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/28 16:30:46 by takumasaoka       #+#    #+#             */
-/*   Updated: 2023/01/30 20:17:30 by takumasaoka      ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "minishell.h"
 #include "exec.h"
 
-int executer(int argc, char **parsed_line, char **envp)
+int	executer(int argc, char **parsed_line, char **envp)
 {
-  char    *command_path;
-  pid_t       pid;
+char	*command_path;
+pid_t	pid;
+int		status;
 
-  command_path = ft_strjoin("/bin/", parsed_line[0]);
-  (void)argc;
-  pid = fork();
-  int         status;
-  
-  if (-1 == pid)
-    err (EXIT_FAILURE, "can not fork");
-  else if (0 == pid)
-  {   
-    //子プロセス
-    if (execve(command_path, parsed_line, envp) == -1){}
-        exit(EXIT_SUCCESS);
-  }
-  else
-  {
-    //親プロセス
-    wait(&status);
-    //ゾンビプロセス回避
-    waitpid(pid, &status, 0);
-  }
-  return 0;
+command_path = ft_strjoin("/bin/", parsed_line[0]);
+(void)argc;
+pid = fork();
+if (-1 == pid)
+	err (EXIT_FAILURE, "can not fork");
+else if (0 == pid)
+{
+	//子プロセス
+	if (execve(command_path, parsed_line, envp) == -1){}
+		exit(EXIT_SUCCESS);
+}
+else
+{
+	//親プロセス
+	wait(&status);
+	//ゾンビプロセス回避
+	waitpid(pid, &status, 0);
+}
+return (0);
 }
