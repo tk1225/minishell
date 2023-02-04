cd6c384c7a89113b6b740386904ba512356b7098
Ashes110 <atsu110121@gmail.com>
2023-02-04 17:41:52 +0900

    pipe中

--- src/executer.c
+++ src/executer.c
@@ -19,57 +19,86 @@
 // 	exec_tree(tree->right, envp);
 // }
 
+// int	executer(char **parsed_line, char **envp)
+// {
+// 	char	*command_path;
+
+// 	command_path = ft_strjoin("/bin/", parsed_line[0]);
+// 	if (execve(command_path, parsed_line, envp) == -1)
+// 		exit(EXIT_SUCCESS);
+// 	return (0);
+// }
+
 int	executer(char **parsed_line, char **envp)
 {
-	char	*command_path;
+	char *path = getenv("PATH");
+    if (path == NULL) {
+        perror("PATH environment variable not set");
+        return 1;
+    }
 
-	command_path = ft_strjoin("/bin/", parsed_line[0]);
-	printf("%s\n", command_path);
-	if (execve(command_path, parsed_line, envp) == -1)
-		exit(EXIT_SUCCESS);
+    size_t path_len = ft_strlen(path);
+    char *path_copy = malloc(path_len + 1);
+    strcpy(path_copy, path);
+
+    char *dir = strtok(path_copy, ":");
+    while (dir != NULL) {
+        // size_t dir_len = strlen(dir);
+        char exec_path[1024];
+        snprintf(exec_path, sizeof(exec_path), "%s/%s", dir, parsed_line[0]);
+
+        if (access(exec_path, X_OK) == 0) {
+            // char *const args[] = {WC_EXECUTABLE, WC_OPTIONS, NULL};
+            execve(exec_path, parsed_line, envp);
+        }
+        dir = strtok(NULL, ":");
+    }
+
+    perror("execve failed");
+    return 1;
 	return (0);
 }
+
+
 
 int	exec_recursion(t_tree *tree, char **envp)
 {
 	//pipe見つけたら左コマンド用にforkして子プロセスで
 	//execveして標準出力をpipeで親プロセスの標準入力で受け取る。
+	// (void)tree;
+	// (void)envp;
+
 	int pipefd[2];
-	(void)envp;
 	if (pipe(pipefd) == -1)
-		return 1;
+		return (1);
 	int pid1 = fork();
 	if (pid1 < 0)
-		return 2;
-	printf("pid1%d\n", pid1);
+		return (2);
 	if (pid1 == 0)
 	{
-		//child
 		dup2(pipefd[1], STDOUT_FILENO);
-		printf("%s\n", tree->left->com[0]);
-		printf("%s\n", tree->left->com[1]);
 		close(pipefd[0]);
 		close(pipefd[1]);
 		//tree->leftがコマンドだったらexecve
 		//tree->left pipeだったらさいき
-		execlp("ping", "ping", "-c", "5", "google.com", NULL);
-		// executer(tree->left->com, envp);
+		executer(tree->left->com, envp);
+		// execve(ft_strjoin("/bin/", tree->left->com[0]), tree->left->com, envp);
+		// execlp("echo", "echo", "test2", NULL);
 	}
 
 	int pid2 = fork();
 	if (pid2 < 0)
-		return 3;
-
+		return (3);
 	if (pid2 == 0)
 	{
-		//child
 		dup2(pipefd[0], STDIN_FILENO);
 		close(pipefd[0]);
 		close(pipefd[1]);
 		//tree->leftがコマンドだったらexecve
 		//tree->left pipeだったらさいき
-		execlp("grep", "grep", "icmp_seq=1", NULL);
-		// executer(tree->right->com, envp);
+		executer(tree->right->com, envp);
+		// execve(ft_strjoin("/usr/bin/", tree->right->com[0]), tree->right->com, envp);
+		// execlp("wc", "wc", "-l", NULL);
 	}
 	close(pipefd[0]);
 	close(pipefd[1]);
--- src/main.c
+++ src/main.c
@@ -4,7 +4,6 @@ int main(int argc, char **argv, char **envp)
 {
 	(void)argc;
 	(void)argv;
-	(void)envp;
 	char *line = NULL;
 	t_tree **tree;
 
