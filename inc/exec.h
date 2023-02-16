8b6fa762e8629705797215aed99c1cf5da407a94
Ashes110 <atsu110121@gmail.com>
2023-02-16 21:42:20 +0900

    exec

--- inc/exec.h
+++ inc/exec.h
@@ -1,11 +1,14 @@
 #ifndef EXEC_H
 # define EXEC_H
 
-int		exec_set(char **com);
+int	exec_set(char **com, char **envp);
 
-int		exec_pwd(void);
-int		exec_echo(char **com);
-int		exec_env(void);
-int		exec_export(char **com);
+int	exec_cd(char **com, char **envp);
+int	exec_echo(char **com, char **envp);
+int	exec_env(char **com, char **envp);
+int	exec_exit(char **com, char **envp);
+int	exec_export(char **com, char **envp);
+int	exec_pwd(char **com, char **envp);
+int	exec_unset(char **com, char **envp);
 
 #endif
--- src/builtin/exec_pwd.c
+++ src/builtin/exec_pwd.c
@@ -1,9 +1,11 @@
 #include "minishell.h"
 
-int	exec_pwd(void)
+int	exec_pwd(char **com, char **envp)
 {
 	char	*path_pwd;
 
+	(void)com;
+	(void)envp;
 	path_pwd = getenv("PWD");
 	ft_putendl_fd(path_pwd, 1);
 	return (SUCCESS);
