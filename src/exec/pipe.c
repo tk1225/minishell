d5a91fd6b3566eed71ea004ecf30ab95e0107e4d
Takumasa Okamoto <takumasaokamoto@TakumasanoMacBook-Pro.local>
2023-03-02 01:38:26 +0900

    2pipeに対応

--- src/exec/pipe.c
+++ src/exec/pipe.c
@@ -1,60 +1,85 @@
 #include "minishell.h"
 
+int count_pipe(t_tree *tree)
+{
+    if (tree->stat == PIPE)
+        return (1 + count_pipe(tree->left) + count_pipe(tree->right));
+    else
+        return (0);
+}
+
 int handle_pipe(t_tree *tree, t_env **envp)
 {
-    int pfd[2][2];
+    int pipefd[4096][2];
     pid_t pid1, pid3;
-    // pid_t pid1, pid2, pid3;
+    int pipe_count;
 
-    if (pipe(pfd[0]) == -1 || pipe(pfd[1]) == -1) {
-        perror("pipe");
-        exit(EXIT_FAILURE);
+    pipe_count = count_pipe(tree);
+    if (pipe_count == 0)
+        return (1);
+
+    int j;
+    j = 0;
+    while (j < pipe_count)
+    {
+        pipe(pipefd[j]);
+        //pipeerror
+        j ++;
     }
-    //最初のみ
+    // if (pipe(pipefd[0]) == -1 || pipe(pipefd[1]) == -1) {
+    //     perror("pipe");
+    //     exit(EXIT_FAILURE);
+    // }
+    //最初のみ最後のコマンド
     pid1 = fork();
     if (pid1 == -1) {
         perror("fork");
         exit(EXIT_FAILURE);
     }
     if (pid1 == 0) { // child process 3
-        dup2(pfd[1][0], STDIN_FILENO);
-        close(pfd[0][0]);
-        close(pfd[0][1]);
-        close(pfd[1][0]);
-        close(pfd[1][1]);
+        dup2(pipefd[pipe_count - 1][READ], STDIN_FILENO);
+        // close(pipefd[0][0]);
+        // close(pipefd[0][1]);
+        close(pipefd[pipe_count - 1][0]);
+        close(pipefd[pipe_count - 1][1]);
 		executer(tree->right->com, envp);
         exit(EXIT_FAILURE);
     }
 
     //pipeの右側(最後以外の中間)
-    int i = 0;
+    int i = pipe_count - 1;
+    // int i = 0;
     char **com;
     t_tree *p_tree;
-    p_tree = tree->left;
-    com = tree->left->right->com;
 
-    while (1)
+    if (tree->left->stat != COM)
     {
-        pid_t pid = fork();
-        if (pid == -1) {
-            perror("fork");
-            exit(EXIT_FAILURE);
-        }
-        if (pid == 0)
-        {
-            dup2(pfd[i][0], STDIN_FILENO);
-            dup2(pfd[i + 1][1], STDOUT_FILENO);
-            close(pfd[i][0]);
-            close(pfd[i][1]);
-            close(pfd[i + 1][0]);
-            close(pfd[i + 1][1]);
-            executer(com, envp);
-            exit(EXIT_FAILURE);
-        }
         p_tree = tree->left;
-        i++;
-        if (p_tree->left->stat == COM)
-            break;
+        com = tree->left->right->com;
+
+        while (1)
+        {
+            pid_t pid = fork();
+            if (pid == -1) {
+                perror("fork");
+                exit(EXIT_FAILURE);
+            }
+            if (pid == 0)
+            {
+                dup2(pipefd[i - 1][READ], STDIN_FILENO);
+                dup2(pipefd[i][WRITE], STDOUT_FILENO);
+                close(pipefd[i - 1][0]);
+                close(pipefd[i - 1][1]);
+                close(pipefd[i][0]);
+                close(pipefd[i][1]);
+                executer(com, envp);
+                exit(EXIT_FAILURE);
+            }
+            p_tree = tree->left;
+            i--;
+            if (p_tree->left->stat == COM)
+                break;
+        }
     }
     //pipeの左側がコマンドの場合(最初)
     pid3 = fork();
@@ -65,20 +90,29 @@ int handle_pipe(t_tree *tree, t_env **envp)
     }
 
     if (pid3 == 0) { // child process 1
-        dup2(pfd[0][1], STDOUT_FILENO);
-        close(pfd[0][0]);
-        close(pfd[0][1]);
-        close(pfd[1][0]);
-        close(pfd[1][1]);
-		executer(tree->left->left->com, envp);
+        dup2(pipefd[0][WRITE], STDOUT_FILENO);
+        close(pipefd[0][0]);
+        close(pipefd[0][1]);
+        close(pipefd[1][0]);
+        close(pipefd[1][1]);
+		executer(com, envp);
+		// executer(tree->left->left->com, envp);
         exit(EXIT_FAILURE);
     }
 
-    close(pfd[0][0]);
-    close(pfd[0][1]);
-    close(pfd[1][0]);
-    close(pfd[1][1]);
-
+    int tmp;
+    tmp = 0;
+    while (tmp < pipe_count)
+    {
+        close(pipefd[tmp][0]);
+        close(pipefd[tmp][1]);
+        tmp ++;
+    }
+    // close(pipefd[0][0]);
+    // close(pipefd[0][1]);
+    // close(pipefd[1][0]);
+    // close(pipefd[1][1]);
+    // free(pipefd);
     // waitpid(pid1, &status, 0);
     // waitpid(pid2, &status, 0);
     // waitpid(pid3, &status, 0);
