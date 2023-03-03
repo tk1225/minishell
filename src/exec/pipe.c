a66ee51022f3bcde54c63071add7bf247fad5b06
Takumasa Okamoto <takumasaokamoto@TakumasanoMacBook-Pro.local>
2023-03-03 11:44:21 +0900

    refact

--- src/exec/pipe.c
+++ src/exec/pipe.c
@@ -56,31 +56,24 @@ int handle_pipe(t_tree *tree, t_env **envp)
     char **com;
     t_tree *p_tree;
     p_tree = tree->left;
-    if (p_tree->stat != COM)
+    while (p_tree->stat != COM)
     {
-        while (1)
-        {
-            com = p_tree->right->com;
-            // ft_putstr_fd(com[0], 1);
-            pid_t pid = fork();
-            if (pid == -1) {
-                perror("fork");
-                exit(EXIT_FAILURE);
-            }
-            if (pid == 0)
-            {
-                dup2(pipefd[i - 1][READ], STDIN_FILENO);
-                dup2(pipefd[i][WRITE], STDOUT_FILENO);
-                close_pipe(pipe_count, pipefd);
-                executer(com, envp);
-                exit(EXIT_FAILURE);
-            }
-            p_tree = p_tree->left;
-            i--;
-
-            if (p_tree->stat == COM)
-                break;
+        com = p_tree->right->com;
+        pid_t pid = fork();
+        if (pid == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
         }
+        if (pid == 0)
+        {
+            dup2(pipefd[i - 1][READ], STDIN_FILENO);
+            dup2(pipefd[i][WRITE], STDOUT_FILENO);
+            close_pipe(pipe_count, pipefd);
+            executer(com, envp);
+            exit(EXIT_FAILURE);
+        }
+        p_tree = p_tree->left;
+        i--;
     }
     com = p_tree->com;
     //pipeの左側がコマンドの場合(最初)
@@ -98,85 +91,3 @@ int handle_pipe(t_tree *tree, t_env **envp)
     close_pipe(pipe_count, pipefd);
     return 0;
 }
-
-
-// pid2 = fork();
-
-    // if (pid2 == -1) {
-    //     perror("fork");
-    //     exit(EXIT_FAILURE);
-    // }
-
-    // if (pid2 == 0) { // child process 2
-    //     dup2(pfd[0][0], STDIN_FILENO);
-    //     dup2(pfd[1][1], STDOUT_FILENO);
-    //     close(pfd[0][0]);
-    //     close(pfd[0][1]);
-    //     close(pfd[1][0]);
-    //     close(pfd[1][1]);
-	// 	executer(tree->left->right->com, envp);
-    //     exit(EXIT_FAILURE);
-    // }
-
-// int handle_pipe(t_tree *tree, t_env **envp)
-// {
-//     int pipefd[2][2];
-//     pid_t pid1, pid2, pid3;
-
-//     if (pipe(pipefd[0][0]) == -1) {
-//         perror("pipe");
-//         exit(EXIT_FAILURE);
-//     }
-
-//     pid1 = fork();
-//     if (pid1 == -1) {
-//         perror("fork");
-//         exit(EXIT_FAILURE);
-//     } else if (pid1 == 0) {
-//         /* First child process executes "ls" command */
-//         close(pipefd[0][0]); /* Close unused read end of the pipe */
-//         dup2(pipefd[0][1], STDOUT_FILENO); /* Redirect stdout to the write end of the pipe */
-//         // if (tree->left->stat == COM)
-// 		executer(tree->right->com, envp);
-//         // execlp("ls", "ls", NULL);
-//         perror("exec ls");
-//         exit(EXIT_FAILURE);
-//     }
-//     pid2 = fork();
-//     if (pid2 == -1) {
-//         perror("fork");
-//         exit(EXIT_FAILURE);
-//     } else if (pid2 == 0) {
-//         /* Second child process executes "grep" command */
-//         close(pipefd[0][1]); /* Close unused write end of the pipe */
-//         dup2(pipefd[0][0], STDIN_FILENO); /* Redirect stdin to the read end of the pipe */
-//         executer(tree->right->com, envp);
-//         perror("exec grep");
-//         exit(EXIT_FAILURE);
-//     }
-
-//     pid3 = fork();
-//     if (pid3 == -1) {
-//         perror("fork");
-//         exit(EXIT_FAILURE);
-//     } else if (pid3 == 0) {
-//         /* First child process executes "ls" command */
-//         close(pipefd[0][0]); /* Close unused read end of the pipe */
-//         dup2(pipefd[0][1], STDOUT_FILENO); /* Redirect stdout to the write end of the pipe */
-//         // if (tree->left->stat == COM)
-// 		executer(tree->left->left->com, envp);
-//         // execlp("ls", "ls", NULL);
-//         perror("exec ls");
-//         exit(EXIT_FAILURE);
-//     }
-//         /* Parent process */
-//         close(pipefd[0][0]); /* Close unused read end of the pipe */
-//         close(pipefd[0][1]); /* Close unused write end of the pipe */
-//         close(pipefd[1][0]); /* Close unused write end of the pipe */
-//         close(pipefd[1][1]); /* Close unused write end of the pipe */
-//         // waitpid(pid1, NULL, 0); /* Wait for the first child process to exit */
-//         // waitpid(pid2, NULL, 0); /* Wait for the second child process to exit */
-//     // }
-
-//     return 0;
-// }
