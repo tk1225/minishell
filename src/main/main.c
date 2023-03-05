528845e49db1183027cc3a5dd8eb632c23f88966
Takumasa Okamoto <takumasaokamoto@TakumasanoMacBook-Pro.local>
2023-03-05 14:20:21 +0900

    mainのnorm修正

--- src/main/main.c
+++ src/main/main.c
@@ -12,20 +12,43 @@ t_env	*env_struct(char **envp)
 	tmp = (t_env *)alloc_exit(sizeof(t_env), 1);
 	out = tmp;
 	tmp->prev = NULL;
-	tmp->key = ft_substr(envp[cnt], 0, ft_strlen(envp[cnt]) - ft_strlen(ft_strchr(envp[cnt], '=')));
-	tmp->value = ft_substr(envp[cnt], ft_strlen(tmp->key) + 1, ft_strlen(envp[cnt]) - ft_strlen(tmp->key) - 1);
+	tmp->key = ft_substr(envp[cnt], 0, ft_strlen(envp[cnt]) - \
+	ft_strlen(ft_strchr(envp[cnt], '=')));
+	tmp->value = ft_substr(envp[cnt], ft_strlen(tmp->key) + 1, \
+	ft_strlen(envp[cnt]) - ft_strlen(tmp->key) - 1);
 	cnt += 1;
 	while (envp[cnt])
 	{
 		tmp->next = (t_env *)alloc_exit(sizeof(t_env), 1);
 		tmp->next->prev = tmp;
 		tmp = tmp->next;
-		tmp->key = ft_substr(envp[cnt], 0, ft_strlen(envp[cnt]) - ft_strlen(ft_strchr(envp[cnt], '=')));
-		tmp->value = ft_substr(envp[cnt], ft_strlen(tmp->key) + 1, ft_strlen(envp[cnt]) - ft_strlen(tmp->key) - 1);
+		tmp->key = ft_substr(envp[cnt], 0, ft_strlen(envp[cnt]) - \
+		ft_strlen(ft_strchr(envp[cnt], '=')));
+		tmp->value = ft_substr(envp[cnt], ft_strlen(tmp->key) + 1, \
+		ft_strlen(envp[cnt]) - ft_strlen(tmp->key) - 1);
 		cnt += 1;
 	}
 	tmp->next = NULL;
 	return (out);
+}
+
+void	check_heredoc(char **res)
+{
+	size_t	cnt;
+	char	*delimiter;
+
+	cnt = 0;
+	while (res[cnt])
+	{
+		if (ft_strncmp(res[cnt], "<<", 3) == 0)
+		{
+			delimiter = res[cnt + 1];
+			if (delimiter == NULL)
+				exit(2);
+			read_heredoc(delimiter);
+		}
+		cnt += 1;
+	}
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -33,6 +56,7 @@ int	main(int argc, char **argv, char **envp)
 	char	*line;
 	t_tree	**tree;
 	t_env	*env;
+	char	**res;
 
 	(void)argv;
 	(void)argc;
@@ -49,23 +73,9 @@ int	main(int argc, char **argv, char **envp)
 			g_status = 1;
 		if (ft_strlen(line) > 0)
 		{
-			char	**res;
 			add_history(line);
 			res = lexer(line);
-			size_t	cnt;
-			char *delimiter;
-			cnt = 0;
-			while (res[cnt])
-			{
-				if (ft_strncmp(res[cnt], "<<", 3) == 0)
-				{
-					delimiter = res[cnt + 1];
-					if (delimiter == NULL)
-						exit(2);
-					read_heredoc(delimiter);
-				}
-				cnt += 1;
-			}
+			check_heredoc(res);
 			tree = parser(res);
 			if (syntax_check(*tree) > 0 && !((*tree)->com && !(*tree)->com[0]))
 			{
