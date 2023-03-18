3039bf695b60c2f2fd653c79f1f82f716f547747
Ashes110 <atsu110121@gmail.com>
2023-03-18 13:28:03 +0900

    clean folder

--- setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-curl -fsSL https://raw.githubusercontent.com/mfunyu/config/main/42homebrew_install.sh | zsh
-brew install readline
-echo "set echo-control-characters off" > ~/.inputrc
--- tester2/test_path/test2/hello.c
+++
@@ -1,7 +0,0 @@
-#include <unistd.h>
-
-int	main()
-{
-    write(1, "test2", 5);
-    return (0);
-}
